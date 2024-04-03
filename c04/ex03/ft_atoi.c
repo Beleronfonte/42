f2c51adee979e397e39b63e300d4d7b5d3115135
Oriol Fernandez Sanchez <ofernand@cbr10s3.42barcelona.com>
2024-04-03 13:49:43 +0200

    Fix: c04ex03 atoi

--- c04/ex03/ft_atoi.c
+++ c04/ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: ofernand <ofernand@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 13:23:24 by ofernand          #+#    #+#             */
-/*   Updated: 2024/04/02 16:55:40 by ofernand         ###   ########.fr       */
+/*   Updated: 2024/04/03 13:47:35 by ofernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@ int	ft_atoi(char *str)
 
 	i = 0;
 	sign = 1;
-	while (str[i] != '\0' && str[i] < 48)
+	while (str[i] != '\0' && ((str[i] > 8 && str[i] < 14) || str[i] == 32))
+		i++;
+	while (str[i] != '\0' && (str[i] == 43 || str[i] == 45))
 	{
 		if (str[i] == '-')
 			sign = -sign;
