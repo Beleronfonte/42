b9f6a61786fdc3a2be4fb2017c211444233f533f
Oriol Fernandez Sanchez <ofernand@cbr10s3.42barcelona.com>
2024-04-03 13:54:53 +0200

    Fix: c04ex04 putnbr_base

--- c04/ex04/ft_putnbr_base.c
+++ c04/ex04/ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: ofernand <ofernand@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 14:56:45 by ofernand          #+#    #+#             */
-/*   Updated: 2024/04/02 17:41:53 by ofernand         ###   ########.fr       */
+/*   Updated: 2024/04/03 13:53:22 by ofernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,9 @@ int	ft_error(char *base)
 	while (base[length] != '\0')
 	{
 		i = length + 1;
-		if (base[length] == 43 || base[length] == 45 || base[length] == 32)
+		if (base[length] == 43 || base[length] == 47 || base[length] < 32)
+			return (1);
+		if (base[length] > 8 && base[length] < 14)
 			return (1);
 		while (base[i] != '\0')
 		{
@@ -37,32 +39,29 @@ int	ft_error(char *base)
 		return (0);
 }
 
-/*needs tsting for ut this inside the whole function*/
-void	ft_min_int(long min, int length, char *base)
-{
-	ft_putnbr_base((min / length), base);
-}
-
 void	ft_putnbr_base(int nbr, char *base)
 {
-	int	length;
-	int	error;
-	int	c;
-	
+	int		length;
+	int		error;
+	long	c;
+
 	error = ft_error(base);
 	length = 0;
 	while (base[length] != '\0')
 		length++;
 	if (error != 1)
 	{
-		if (nbr < 0)
+		if (nbr == -2147483648)
+		{
+			c = nbr;
+			ft_putnbr_base((c / length), base);
+		}
+		else if (nbr < 0)
 		{
 			write(1, "-", 1);
-			if (nbr == -2147483648)
-				ft_min_int(2147483648, length, base);
 			nbr = -nbr;
 		}
-		if (nbr > length)
+		if (nbr >= length)
 			ft_putnbr_base((nbr / length), base);
 		c = (nbr % length);
 		write(1, &base[c], 1);
