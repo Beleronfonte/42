436af8d2f86cc0ef88db9d3044e8e175ef49abe6
Oriol Fernandez <oriol.fersan@gmail.com>
2024-04-19 12:21:27 +0200

    Doc: ft_memcpy.c

---
+++ libft/ft_memcpy.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ofernand <ofernand@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/19 11:17:31 by ofernand          #+#    #+#             */
+/*   Updated: 2024/04/19 12:22:18 by ofernand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+//#include <stdio.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t			i;
+
+	if (src == NULL || dest == NULL)
+		return (NULL);
+	i = 0;
+	while (i < n)
+	{
+		((char *)dest)[i] = ((const char *)src)[i];
+		i++;
+	}
+	return (dest);
+}
+
+/*int	main(void)
+{
+	char	*test;
+	char	*ptr;
+ 
+	test = NULL; 
+	ptr = "HOLA"; 
+	ft_memcpy(test, ptr, 2);
+	printf("%s\n", test);
+}*/
