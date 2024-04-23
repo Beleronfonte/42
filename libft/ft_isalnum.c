8181140e90a02bca20bf61c31c068f9a23dc0946
Oriol Fernandez <oriol.fersan@gmail.com>
2024-04-23 13:51:54 +0200

    Doc: isalnum, isalpha, isprint, tolower

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -1,25 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ofernand <ofernand@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/18 11::42 by ofernand          #+#    #+#             */
+/*   Created: 2024/04/18 11:11:42 by ofernand          #+#    #+#             */
 /*   Updated: 2024/04/18 12:22:38 by ofernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
-int		ft_atoi(const char *str);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-void	*ft_memset(void *str, int c, size_t n);
-char	*ft_strchr(const char *s, int n);
-size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-char	*ft_strnstr(const char *big, const char *little, size_t len);
-int		ft_toupper(int c);
+int	ft_isalnum(int c)
+{
+	return (ft_isalpha(c) || ft_isdigit(c));
+}
