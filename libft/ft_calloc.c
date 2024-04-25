33168d76e452b65acc5a446b91fa23ec933f83d3
Oriol Fernandez <oriol.fersan@gmail.com>
2024-04-25 12:20:13 +0200

    Doc: calloc

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -1,20 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_tolower.c                                       :+:      :+:    :+:   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ofernand <ofernand@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/26 16:17:31 by ofernand          #+#    #+#             */
-/*   Updated: 2024/03/26 16:22:18 by ofernand         ###   ########.fr       */
+/*   Created: 2024/04/18 11:11:42 by ofernand          #+#    #+#             */
+/*   Updated: 2024/04/18 12:22:38 by ofernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_tolower(int c)
+void	*calloc(size_t n, size_t size)
 {
-	if (c >= 'A' && c <= 'Z')
-		c = c + 32;
-	return (c);
+	void	*ptr;
+
+	ptr = malloc(n * size);
+	if (ptr == NULL)
+		return (ptr);
+	ft_bzero(ptr, n * size);
+	return (ptr);
 }
