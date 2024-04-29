4825b4856c87eb622a777826e8065cb0f58880cb
Oriol Fernandez <oriol.fersan@gmail.com>
2024-04-29 12:26:38 +0200

    Doc: substr

---
+++ libft/ft_substr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ofernand <ofernand@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 11:26:27 by ofernand          #+#    #+#             */
+/*   Updated: 2024/04/29 12:25:51 by ofernand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub_s;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+		len = 0;
+	if (ft_strlen(s) < len)
+		len = ft_strlen(s);
+	sub_s = (char *)malloc(sizeof(char) * (len + 1));
+	if (sub_s == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		sub_s[i] = s[len + i];
+		i++;
+	}
+	sub_s[i] = '\0';
+	return (sub_s);
+}
