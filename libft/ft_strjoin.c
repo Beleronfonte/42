0b533c8feea213b88bb040ca6a383e868acbcf25
Oriol Fernandez <oriol.fersan@gmail.com>
2024-04-29 14:09:19 +0200

    Doc: strjoin

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,8 +6,37 @@
 /*   By: ofernand <ofernand@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 11:55:59 by ofernand          #+#    #+#             */
-/*   Updated: 2024/04/29 11:56:17 by ofernand         ###   ########.fr       */
+/*   Updated: 2024/04/29 13:12:51 by ofernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+//#include <stdio.h>
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	len1;
+	size_t	len2;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	str = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+	if (str == NULL)
+		return (NULL);
+	ft_strlcpy(str, s1, len1 + 1);
+	ft_strlcat(str, s2, len1 + len2 + 1);
+	return (str);
+}
+
+/*int	main(void)
+{
+	char	*s1;
+	char	*s2;
+	char	*str;
+
+	s1 = "Hello";
+	s2 = "World";
+	str = ft_strjoin(s1, s2);
+	printf("%s", str);
+}*/
