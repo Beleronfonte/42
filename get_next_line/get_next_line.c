a3727dc5b056741fb0439fe31a9f9e4ede3ba20c
Oriol Fernandez <oriol.fersan@gmail.com>
2024-05-29 11:55:20 +0200

    Doc: it works only with BUFFER >= line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ofernand <ofernand@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 12:02:29 by ofernand          #+#    #+#             */
-/*   Updated: 2024/05/20 10:38:59 by ofernand         ###   ########.fr       */
+/*   Updated: 2024/05/29 11:48:50 by ofernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,26 @@ char	*get_next_line(int fd)
 	char		*new_line;
 	static char	new_char[1];
 	int			i;
+	int			err_ctrl;
 
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
 	i = 0;
 	new_line = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (new_char[0] == '\n')
-		new_char[0] = 1;
+		new_char[0] = ' ';
 	while (i <= BUFFER_SIZE && new_char[0] != '\n')
 	{
-		read(fd, new_char, 1);
+		err_ctrl = read(fd, new_char, 1);
+		if (err_ctrl == -1)
+			return (NULL);
+		if (err_ctrl == 0 && i == 0)
+			return (NULL);
+		if (err_ctrl == 0 && i != 0)
+			return (new_line);
 		new_line[i] = new_char[0];
-		if (new_char[0] == '\0')
-			break;
 		i++;
 	}
-	if (new_char[0] == '\0' && i == 0)
-		return (NULL);
-	new_line[i] = '\0';
 	return (new_line);
 }
 
@@ -42,18 +46,6 @@ int	main(void)
 	char	*to_print;
 
 	fd = open("./numbers.dict", O_RDONLY);
-	to_print = get_next_line(fd);
-	printf("%s", to_print);
-	to_print = get_next_line(fd);
-	printf("%s", to_print);
-	to_print = get_next_line(fd);
-	printf("%s", to_print);
-	to_print = get_next_line(fd);
-	printf("%s", to_print);
-	to_print = get_next_line(fd);
-	printf("%s", to_print);
-	to_print = get_next_line(fd);
-	printf("%s", to_print);
 	to_print = get_next_line(fd);
 	printf("%s", to_print);
 	free(to_print);
